259a481f64446e4fcab9522868238f7b2aef28c1
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-04-15 20:59:43 +0300

    Initial Commit

---
+++ MyMealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMealTime
+//
+//  Created by Marina on 15.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
