7bfb7179419853dce7ed3b274e95a8c40b068d21
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-07-11 19:48:31 +0300

    ViewController,CoreData

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MyMealTime
+//
+//  Created by Marina on 15.04.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MyMealTime
+//
+//  Created by Marina on 15.04.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
